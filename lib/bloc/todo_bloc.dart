6050a3c3666a79dd8a31f8ab137af3a7aeb98061
ulumdev <belajarkoding97@gmail.com>
2025-05-09 18:25:05 +0700

    refactor extract bloc event

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app_bloc/models/todo.dart';
@@ -7,30 +8,36 @@ part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoBloc() : super(const TodoInitial([])) {
-    on<OnAddTodo>((event, emit) async {
-      emit(TodoLoading(state.todos));
-      await Future.delayed(const Duration(milliseconds: 1500));
-      Todo newTodo = event.newTodo;
-      emit(TodoAdded([...state.todos, newTodo]));
-    });
+    on<OnAddTodo>(addTodo);
 
-    on<OnUpdateTodo>((event, emit) async {
-      emit(TodoLoading(state.todos));
-      await Future.delayed(const Duration(milliseconds: 1500));
-      int index = event.index;
-      Todo newTodo = event.updatedTodo;
-      List<Todo> todosUpdated = state.todos;
-      todosUpdated[index] = newTodo;
-      emit(TodoUpdate(todosUpdated));
-    });
+    on<OnUpdateTodo>(updateTodo);
 
-    on<OnRemoveTodo>((event, emit) async {
-      emit(TodoLoading(state.todos));
-      await Future.delayed(const Duration(milliseconds: 1500));
-      int index = event.index;
-      List<Todo> todosRemoved = state.todos;
-      todosRemoved.removeAt(index);
-      emit(TodoRemove(todosRemoved));
-    });
+    on<OnRemoveTodo>(removeTodo);
+  }
+
+  FutureOr<void> addTodo(OnAddTodo event, Emitter<TodoState> emit) async {
+    emit(TodoLoading(state.todos));
+    await Future.delayed(const Duration(milliseconds: 1500));
+    Todo newTodo = event.newTodo;
+    emit(TodoAdded([...state.todos, newTodo]));
+  }
+
+  FutureOr<void> updateTodo(OnUpdateTodo event, Emitter<TodoState> emit) async {
+    emit(TodoLoading(state.todos));
+    await Future.delayed(const Duration(milliseconds: 1500));
+    int index = event.index;
+    Todo newTodo = event.updatedTodo;
+    List<Todo> todosUpdated = state.todos;
+    todosUpdated[index] = newTodo;
+    emit(TodoUpdate(todosUpdated));
+  }
+
+  FutureOr<void> removeTodo(OnRemoveTodo event, Emitter<TodoState> emit) async {
+    emit(TodoLoading(state.todos));
+    await Future.delayed(const Duration(milliseconds: 1500));
+    int index = event.index;
+    List<Todo> todosRemoved = state.todos;
+    todosRemoved.removeAt(index);
+    emit(TodoRemove(todosRemoved));
   }
 }
